00f91a9242d51a8912f3990c355afad7b8ad8bde
MathCarv <matheus.camara@gec.inatel.br>
2022-10-10 13:06:32 -0300

    Redes Sociais - Trabalho

--- src/redessociais/RedesSociais.java
+++ src/redessociais/RedesSociais.java
@@ -9,24 +9,24 @@ public class RedesSociais {
         ArrayList<RedeSocial> redesSociais = new ArrayList<>();
 
         Facebook facebook = new Facebook();
-        facebook.setSenha("77227722");
-        facebook.setNumAmigos(2560);
+        facebook.setSenha("512315");
+        facebook.setNumAmigos(1523);
         redesSociais.add(facebook);
 
         Instagram instagram = new Instagram();
-        instagram.setSenha("162055893");
-        instagram.setNumAmigos(965);
+        instagram.setSenha("6123123");
+        instagram.setNumAmigos(123);
         redesSociais.add(instagram);
 
         Usuario usuario = new Usuario(redesSociais);
-        usuario.setNome("Gustavo Simoes");
-        usuario.setEmail("gustavoteixeira@gec.inatel.br");
+        usuario.setNome("Matheus Camara Carvalho");
+        usuario.setEmail("matheus.camara@gec.inatel.br");
         usuario.mostraInfo();
 
         for (int i = 0; i < usuario.usa.size(); i++) {
             if (usuario.usa.get(i) instanceof Facebook) {
                 Facebook fb = (Facebook) usuario.usa.get(i);
-                System.out.println("///////Facebook///////");
+                System.out.println("---------Facebook---------");
                 System.out.println("Numero de Amigos:" + fb.getNumAmigos());
                 System.out.println("Senha:" + fb.getSenha());
                 fb.compartilhar();
@@ -35,16 +35,16 @@ public class RedesSociais {
                 System.out.println("\n");
             } else if (usuario.usa.get(i) instanceof Instagram) {
                 Instagram ig = (Instagram) usuario.usa.get(i);
-                System.out.println("///////Instagram///////");
+                System.out.println("---------Instagram---------");
                 System.out.println("Numero de Amigos:" + ig.getNumAmigos());
                 System.out.println("Senha:" + ig.getSenha());
                 ig.postarComentario();
                 ig.postarVideo();
                 ig.postarFoto();
                 System.out.println("\n");
-            }/* else if (usuario.usa.get(i) instanceof Twitter) {
+            } else if (usuario.usa.get(i) instanceof Twitter) {
                 Twitter tw = (Twitter) usuario.usa.get(i);
-                System.out.println("///////Twitter///////");
+                System.out.println("---------Twitter---------");
                 System.out.println("Numero de Amigos:" + tw.getNumAmigos());
                 System.out.println("Senha:" + tw.getSenha());
                 tw.postarComentario();
@@ -53,14 +53,14 @@ public class RedesSociais {
                 System.out.println("\n");
             } else {
                 GooglePlus gp = (GooglePlus) usuario.usa.get(i);
-                System.out.println("///////Instagram///////");
+                System.out.println("---------Instagram---------");
                 System.out.println("Numero de Amigos:" + gp.getNumAmigos());
                 System.out.println("Senha:" + gp.getSenha());
                 gp.postarComentario();
                 gp.postarVideo();
                 gp.postarFoto();
                 System.out.println("\n");
-            }*/
+            }
         }
 
     }
